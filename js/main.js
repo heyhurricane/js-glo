cc078b3405e7667d3076a8071bd92ef6d3b43725
heyhurricane <dinohka@mail.ru>
2020-11-20 16:59:07 +0400

    variable initialization

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+let income = "Фриланс";
+let addExpenses = "Интернет, Комуналка, Бензин";
+let deposit = false;
+let mission = 150000;
+let period = 6;
 
-alert("Hello world!");
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
 
-console.log("Another text");
+console.log(addExpenses.length);
+
+console.log("Период равен "+ period +" месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase().split(", "));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
