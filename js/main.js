e2b18309862771c4e70bf0eb45231db04d7e3cfd
heyhurricane <dinohka@mail.ru>
2020-11-23 21:20:01 +0400

    add condition and calculations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-let money = 30000;
+"use strict"
+
+let money = prompt("Ваш месячный доход?");
+
 let income = "Фриланс";
-let addExpenses = "Интернет, Комуналка, Бензин";
-let deposit = false;
+let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+let deposit = confirm("Есть ли у вас депозит в банке?");
+let expenses1 = prompt("Введите обязательную статью расходов:");
+let amount1 = prompt("Во сколько это обойдется?", "0");
+let expenses2 = prompt("Введите обязательную статью расходов:");
+let amount2 = prompt("Во сколько это обойдется?", "0");
 let mission = 150000;
 let period = 6;
 
@@ -15,6 +22,22 @@ console.log("Период равен "+ period +" месяцев");
 console.log("Цель заработать " + mission + " рублей");
 
 console.log(addExpenses.toLowerCase().split(", "));
+let budgetMonth = money - amount1 - amount2;
+console.log("Бюджет на месяц: ", budgetMonth);
+console.log("Цель будет достигнута за " + Math.ceil(mission / budgetMonth) + " мес.");
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+let budgetDay = budgetMonth / 30;
+console.log("Бюджет на день: ", Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+  console.log("У вас высокий уровень дохода");
+}
+else {
+  if ((budgetDay >= 600) && (budgetDay < 1200)) { 
+    console.log("У вас средний уровень дохода");
+  }
+  else 
+  {
+    console.log("К сожалению, у вас уровень дохода ниже среднего");
+  }
+}
