df1056d4447e08236d8c062d7d3f3c99ab75b1f1
heyhurricane <dinohka@mail.ru>
2020-12-15 15:29:07 +0400

    fix some bugs

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ window.addEventListener('DOMContentLoaded', function() {
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
 
+    function addZero(hours, minutes, seconds) {
+      if (hours.length === 1) {
+        hours = '0' + hours;
+      }
+      if (minutes.length === 1) {
+        minutes = '0' + minutes;
+      }
+      if (seconds.length === 1) {
+        seconds = '0' + seconds;
+      }
+      return {hours, minutes, seconds};
+    }
+
     function getTimeRemaining() {
       let dateStop = new Date(deadline).getTime(),
           dateNow = new Date().getTime(),
@@ -15,38 +28,27 @@ window.addEventListener('DOMContentLoaded', function() {
           seconds = (Math.floor(timeRemaining % 60)).toString(),
           minutes = (Math.floor((timeRemaining / 60) % 60)).toString(),
           hours = (Math.floor((timeRemaining / 60) / 60)).toString();
+      const time = addZero(hours, minutes, seconds);
+      seconds = time.seconds;
+      minutes = time.minutes;
+      hours = time.hours;
       return {timeRemaining, hours, minutes, seconds};
     }
+
     function updateClock() {
       let timer = getTimeRemaining();
-      if (timer.hours.length === 1) {
-        timer.hours = '0' + timer.hours;
-      }
-      if (timer.minutes.length === 1) {
-        timer.minutes = '0' + timer.minutes;
-      }
-      if (timer.seconds.length === 1) {
-        timer.seconds = '0' + timer.seconds;
-      }
       timerHours.textContent = timer.hours;
       timerMinutes.textContent = timer.minutes;
       timerSeconds.textContent = timer.seconds;
-      if (timer.timeRemaining > 0) {
-        setInterval(updateClock, 1000);
-        // setTimeout(updateClock, 1000);
-      }
-      else {
+      if (timer.timeRemaining <= 0) {
         timerHours.textContent = '00';
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
       }
     }
 
-    updateClock();
+    setInterval(updateClock, 1000);
 
   }
   countTimer('16 Dec 2020');
-
-
-  //setInterval(countTimer, 1000, '31 Dec 2020');
 });
