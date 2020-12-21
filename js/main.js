742847eb27c692492c6ec566546b8eedf2818b0f
heyhurricane <dinohka@mail.ru>
2020-12-21 21:29:19 +0400

    add regular expression

--- js/main.js
+++ js/main.js
@@ -302,4 +302,32 @@ window.addEventListener('DOMContentLoaded', function() {
   };
 
   slider();
+
+  // наведение на картинки
+
+  const images = document.querySelectorAll('.command__photo');
+  images.forEach((elem) => {
+    elem.addEventListener('mouseenter', (event) => {
+      const oldImage = event.target.src;
+      event.target.src = event.target.dataset.img;
+      event.target.dataset.img = oldImage;
+    });
+     elem.addEventListener('mouseleave', (event) => {
+      const newImage = event.target.src;
+      event.target.src = event.target.dataset.img;
+      event.target.dataset.img = newImage;
+    });
+  });
+  
+  // ввод в калькуляторе только цифр
+
+  const inputs = document.querySelectorAll('input.calc-item');
+
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      input.value = input.value.replace(/[^\d]/,'');
+    });
+  });
+
+
 });
